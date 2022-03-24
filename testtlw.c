9b6841f74ae85ced07c367c689f12125ca4d0c8c
Maciej Kuzmicki <macius02@DESKTOP-MKE9L6S.localdomain>
2022-03-24 20:47:51 +0100

    Dodaje generator tlw oraz modul testujacu tlw

---
+++ testtlw.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "tlw.h"
+#include "graf.h"
+
+
+int main(int argc, char **argv){
+	FILE *out = argc > 1 ? fopen(argv[1], "w") :stdin;
+	int a = argc >2 ? atoi(argv[2]) : 7;
+	int b = argc > 3? atoi(argv[3]) : 4;
+	double w1 = argc >4 ? atof(argv[4]) : 0;
+	double w2 = argc >5 ? atof(argv[5]) :1;
+	struct Graph* graph = generate_tlw(a,b,w1,w2);
+        writeGraph(graph, out);
+	return 0;	
+}
